8c95ad4e85d5613cc1ea4dac49e30718672918a0
Nihal <thisisnihal95@gmail.com>
2024-11-10 12:25:18 +0530

    commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -89,7 +89,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href=""
+                  href="https://github.com/Nihalakhare01"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
